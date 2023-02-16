2f61d809b41b791b9f2a67472abd1df395eebeea
itorres8 <ignaciotorresmayobre8@gmail.com>
2023-02-16 17:23:50 -0300

    correccion script

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ const app = express();
 const publicPath = path.resolve(__dirname, './public');
 app.use( express.static(publicPath) );
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo');
-});
 
 app.get('/', (req,res) =>{
     res.sendFile(path.resolve(__dirname, './views/home.html'));
@@ -19,3 +16,9 @@ app.get('/', (req,res) =>{
 app.get('/views/register', (req,res) =>{
     res.sendFile(path.resolve(__dirname, './views/register.html'));
 })
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
